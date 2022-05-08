9bef9499c43d03669ce01a5291b3ae0e08dbe9ff
otegami <a.s.takuya1026@gmail.com>
2022-05-09 08:40:06 +0900

    Implemented table

---
+++ src/table.rs
@@ -0,0 +1,22 @@
+pub struct SimpleTable {
+  pub meta_page_id: PageId,
+  pub num_key_elems: usize,
+}
+
+impl SimpleTable {
+  pub fn create(&mut self, bufmgr: &mut BufferPoolManager) -> Result<()> {
+    let btree = BTree::create(bufmgr)?;
+    self.meta_page_id = btree.meta_page_id;
+    Ok(())
+  }
+
+  pub fn insert(&self, bufmgr: &mut BufferPoolManager, record: &[&[u8]]) -> Result<()> {
+    let btree = BTree::new(self.meta_page_id);
+    let mut key = vec![];
+    tuple::encode(record[..self.num_key_elems].iter(), &mut key);
+    let mut value = vec![];
+    tuple::encode(record[self.num_key_elems..].iter(), &mut value);
+    btree.put(bufmgr, &key, &value)?;
+    Ok(())
+  }
+}
